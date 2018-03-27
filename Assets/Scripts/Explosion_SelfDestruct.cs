a769d0f56dcc70c2686367757e470b5fa3798e56
Wudzu <wudzu1@gmail.com>
2018-03-27 18:01:46 +0200

    Wersja 3
    Wersja 3 z google drive

---
+++ Assets/Scripts/Explosion_SelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion_SelfDestruct : MonoBehaviour {
+
+
+    public float TTL = 0.5f; // laser explosion time-to-live
+    
+	// Update is called once per frame
+	void Update () {
+
+        // self-destroy the bulet after time
+        TTL -= Time.deltaTime;
+        if (TTL < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Main_controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main_controller : MonoBehaviour {
+
+    public Text basic;
+    public Text help;
+    public Text Score;
+    bool helpDisplaied = false;
+
+    int ScoreCnt = 0;
+
+    public void AddScore()
+    {
+
+        ScoreCnt++;
+
+        Score.text = "Score: " + ScoreCnt.ToString();
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (helpDisplaied)
+            {
+                basic.enabled = true;
+                help.enabled = false;
+                helpDisplaied = false;
+            }
+            else
+            {
+                basic.enabled = false;
+                help.enabled = true;
+                helpDisplaied = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+            Application.OpenURL(webplayerQuitURL);
+        #else
+            Application.Quit();
+        #endif
+        }
+
+     }
+}
