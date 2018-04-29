0fe222bbfa9c1b85991c38d0ec2911ad49886ea8
Wudzu <wudzu1@gmail.com>
2018-04-29 16:32:16 +0200

    MechState
    Podstawa obrażeń,  gracz powinien mieć obiekt MechState i obrażenia powinny iść na funkcje damage tego obiektu. Trze pozmieniać na public rzeczy (przynajmniej wszystko MechDamage i kilka funkcji w reszcie).

---
+++ Assets/Mech_State/Scripts/MechArmor.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MechArmor
+{
+	var change   = new Dictionary<string, float>();
+	var reduction = new Dictionary<string, float>();
+	var destruction = new Dictionary<string, float>();
+
+	public enum A_Type { MetalSoft, MetalHard, Ammortization };
+	float structure;
+
+	//var Temperature;
+
+	float mass;
+
+	//var other;
+
+	MechDamage reduct(MechDamage dmg)
+	{
+		
+	}
+
+	MechDamage damage(MechDamage dmg)
+	{
+		dmg.basicDamage["Blunt"] = dmg.basicDamage["Blunt"]
+	}
+
+	void LoadNew(A_Type armor)
+	{
+		switch (armor) {
+		case A_Type.MetalSoft:
+			change ["Blunt_to_Blunt"] = 1.0;
+			change ["Cut_to_Blunt"] = 1.0;
+			change ["Pierce_to_Blunt"] = 1.0;
+			change ["Abrasive_to_Blunt"] = 0.0;
+
+			change ["Blunt_to_Cut"] = 0.0;
+			change ["Cut_to_Cut"] = 0.0;
+			change ["Pierce_to_Cut"] = 0.0;
+			change ["Abrasive_to_Cut"] = 0.0;
+
+			change ["Blunt_to_Pierce"] = 0.0;
+			change ["Cut_to_Pierce"] = 0.0;
+			change ["Pierce_to_Pierce"] = 0.0;
+			change ["Abrasive_to_Pierce"] = 0.0;
+
+			change ["Blunt_to_Abrasive"] = 0.0;
+			change ["Cut_to_Abrasive"] = 0.0;
+			change ["Pierce_to_Abrasive"] = 0.0;
+			change ["Abrasive_to_Abrasive"] = 1.0;
+
+			reduction ["Blunt"] = 0.12;
+			reduction ["Cut"] = 0.0;
+			reduction ["Pierce"] = 0.0;
+			reduction ["Abrasive"] = 0.8;
+
+			destruction ["Blunt"] = 0.1;
+			destruction ["Cut"] = 0.4;
+			destruction ["Pierce"] = 0.4;
+			destruction ["Abrasive"] = 0.5;
+
+			structure = 100.0;
+			mass = 8.0;
+
+			break;
+		
+		case A_Type.MetalHard:
+			change ["Blunt_to_Blunt"] = 1.0;
+			change ["Cut_to_Blunt"] = 1.0;
+			change ["Pierce_to_Blunt"] = 1.0;
+			change ["Abrasive_to_Blunt"] = 0.0;
+
+			change ["Blunt_to_Cut"] = 0.0;
+			change ["Cut_to_Cut"] = 0.0;
+			change ["Pierce_to_Cut"] = 0.0;
+			change ["Abrasive_to_Cut"] = 0.0;
+
+			change ["Blunt_to_Pierce"] = 0.0;
+			change ["Cut_to_Pierce"] = 0.0;
+			change ["Pierce_to_Pierce"] = 0.0;
+			change ["Abrasive_to_Pierce"] = 0.0;
+
+			change ["Blunt_to_Abrasive"] = 0.0;
+			change ["Cut_to_Abrasive"] = 0.0;
+			change ["Pierce_to_Abrasive"] = 0.0;
+			change ["Abrasive_to_Abrasive"] = 1.0;
+
+			reduction ["Blunt"] = 0.1;
+			reduction ["Cut"] = 0.0;
+			reduction ["Pierce"] = 0.0;
+			reduction ["Abrasive"] = 0.6;
+
+			destruction ["Blunt"] = 0.5;
+			destruction ["Cut"] = 0.1;
+			destruction ["Pierce"] = 0.1;
+			destruction ["Abrasive"] = 1.0;
+
+			structure = 100.0;
+			mass = 10.0;
+
+			break;
+
+		case A_Type.Ammortization:
+			change ["Blunt_to_Blunt"] = 1.0;
+			change ["Cut_to_Blunt"] = 0.0;
+			change ["Pierce_to_Blunt"] = 0.0;
+			change ["Abrasive_to_Blunt"] = 0.0;
+
+			change ["Blunt_to_Cut"] = 0.0;
+			change ["Cut_to_Cut"] = 1.0;
+			change ["Pierce_to_Cut"] = 0.0;
+			change ["Abrasive_to_Cut"] = 0.0;
+
+			change ["Blunt_to_Pierce"] = 0.0;
+			change ["Cut_to_Pierce"] = 0.0;
+			change ["Pierce_to_Pierce"] = 1.0;
+			change ["Abrasive_to_Pierce"] = 0.0;
+
+			change ["Blunt_to_Abrasive"] = 0.0;
+			change ["Cut_to_Abrasive"] = 0.0;
+			change ["Pierce_to_Abrasive"] = 0.0;
+			change ["Abrasive_to_Abrasive"] = 1.0;
+
+			reduction ["Blunt"] = 0.8;
+			reduction ["Cut"] = 0.0;
+			reduction ["Pierce"] = 0.0;
+			reduction ["Abrasive"] = 0.0;
+
+			destruction ["Blunt"] = 0.02;
+			destruction ["Cut"] = 0.5;
+			destruction ["Pierce"] = 0.5;
+			destruction ["Abrasive"] = 0.9;
+
+			structure = 100.0;
+			mass = 7.0;
+
+			break;
+		}
+	}
+}
+
---
+++ Assets/Mech_State/Scripts/MechDamage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MechDamage : MonoBehaviour
+{
+	var basicDamage   = new Dictionary<string, float>();
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		basicDamage ["Blunt"] = 0.0;
+		basicDamage ["Cut"] = 0.0;
+		basicDamage ["Pierce"] = 15.0;
+		basicDamage ["Abrasive"] = 0.0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
